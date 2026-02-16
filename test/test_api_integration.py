aa5bb7785461695f18ea6ed9882af13b0514ba3f
Braxton Bonilla <bbonilla@ts4.mx>
2026-02-15 23:51:12 -0500

    test modified for CI/CD purposes

--- test/test_api_integration.py
+++ test/test_api_integration.py
@@ -9,15 +9,32 @@ def test_guidelines_endpoint(mocker):
     Verifica que el endpoint GET /guidelines/ devuelva una lista y status 200.
     """
 
+    mock_db = MagicMock()
+
     mock_guidelines = [
-        MagicMock(success_criterion="1.1.1", description="Texto alternativo", level="A"),
-        MagicMock(success_criterion="1.2.1", description="Solo audio", level="A")
+        MagicMock(id=1,success_criterion="1.1.1", description="Texto alternativo", level="A"),
+        MagicMock(id=2,success_criterion="1.2.1", description="Solo audio", level="A")
     ]
 
+    mock_db.query.return_value.all.return_value = mock_guidelines
+    
+    def override_get_db():
+        try:
+            yield mock_db
+        finally:
+            pass
+    
+    app.dependency_overrides[get_db] = override_get_db
+
     response = client.get("/guidelines/")
+
+    dependency_overrides = {}
 
     assert response.status_code == 200
 
     data = response.json()
-    assert isinstance(data, list)
-    assert len(data) > 0
+    assert len(data) == 2
+    assert data[0]["success_criterion"] == "1.1.1"
+
+    print("\n Test de Integración (Mocking DB) -> PASÓ")
+    
